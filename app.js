a0dd76eb0128f220ec4bd828c0757d03b48b1e65
sinistri <balestri@gmail.com>
2025-03-17 22:41:40 -0300

    se agrega el condicional para que no se repitan los nombres

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ function agregarAmigo() {
         return;
     }
 
+    if (amigo.includes(nombreAmigo)) { //  Evita agregar nombres duplicados
+        alert("Este nombre ya ha sido agregado");
+        return;
+    }
+
     amigo.push(nombreAmigo);
     inputAmigo.value="";
     inputAmigo.focus();
@@ -38,6 +43,8 @@ function sortearAmigo(){
         alert("No hay amigos para sortear");
         return;
     }
+   
+   
     let amigoSorteado =amigo[Math.floor(Math.random() * amigo.length)];
     let resultado = document.getElementById("resultado");
     resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
